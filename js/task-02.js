e78d52a9248a533680550cce65b8e9df6ffcbdc3
Oleksandr-Stepenko <113541212+Oleksandr-Stepenko@users.noreply.github.com>
2023-02-19 16:03:30 +0200

    add

--- js/task-02.js
+++ js/task-02.js
@@ -1,24 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-	'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
 const listEl = document.querySelector("#ingredients");
 
 const ingredientsList = (ingredients) => {
-	return ingredients.map((ingredient) => {
-		const itemEl = document.createElement("li");
-		itemEl.className = "item";
+  return ingredients.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.className = "item";
     itemEl.textContent = ingredient;
-
-	return itemEl;
-	// return listEl.append(itemEl);
-  });}
-
+    return itemEl;
+  });
+};
 
 const elements = ingredientsList(ingredients);
 listEl.append(...elements);
